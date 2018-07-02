d0d589b5cdb9f5f03e180a767721f56d327e31e6
Radoslav Genchev <r.genchew@gmail.com>
2018-07-03 00:37:41 +0200

    scaffold 'Places'

---
+++ db/migrate/20180626192030_create_places.rb
@@ -0,0 +1,9 @@
+class CreatePlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :places do |t|
+      t.references :location, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
