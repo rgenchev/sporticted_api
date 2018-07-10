1cded0593842aed80d5344d9fd6b6de1fe4a90ef
Radoslav Genchev <r.genchew@gmail.com>
2018-07-10 23:51:46 +0200

    update schema

---
+++ db/schema.rb
@@ -0,0 +1,75 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_07_10_212035) do
+
+  create_table "challenges", force: :cascade do |t|
+    t.integer "winner_id"
+    t.integer "loser_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["loser_id"], name: "index_challenges_on_loser_id"
+    t.index ["winner_id"], name: "index_challenges_on_winner_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.text "rules"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hosts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "place_id"
+    t.integer "rating"
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
+    t.index ["place_id"], name: "index_hosts_on_place_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.integer "post_code"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_places_on_location_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.integer "wins"
+    t.integer "losses"
+    t.integer "draws"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "rating"
+  end
+
+end
