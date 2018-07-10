fb30840e4104fe2b6a19312e40c4ca0f6254ca10
Radoslav Genchev <r.genchew@gmail.com>
2018-07-10 23:50:42 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :hosts
+  resources :challenges
+  resources :games
+  resources :places
+  resources :locations
+  resources :records
+  resources :users
+
+  post '/signup',  to: 'users#create'
+
+  post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
