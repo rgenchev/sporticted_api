ea209d89b718a525e712bf07bcd83095a8651fc4
Radoslav Genchev <r.genchew@gmail.com>
2018-07-10 23:51:31 +0200

    create hosts and implement validations

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
+    end
+end
