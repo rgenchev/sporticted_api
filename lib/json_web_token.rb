1060f051a3bb2183ad280a60cc51a0ac16fcf3fe
Radoslav Genchev <r.genchew@gmail.com>
2018-07-26 16:10:30 +0200

    encrypt with temp secret key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
  class << self
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
-     JWT.encode(payload, nil, Rails.application.secrets.secret_key_base)
+     JWT.encode(payload, 'e4d884bb4ff0897611f1b6223120b663396fe768859f200f6ff0edbf99624fe1e2edbf36cfad98bbd6a880d61f73ff4091a5c60a1f161017593ac7e0f9a2a78f', 'HS256')
    end
 
    def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+     body = JWT.decode(token, 'e4d884bb4ff0897611f1b6223120b663396fe768859f200f6ff0edbf99624fe1e2edbf36cfad98bbd6a880d61f73ff4091a5c60a1f161017593ac7e0f9a2a78f', true, { algorithm: 'HS256' })[0]
      HashWithIndifferentAccess.new body
    rescue
      nil
