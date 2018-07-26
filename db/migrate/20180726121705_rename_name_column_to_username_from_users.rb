d2ad9ca2577a013226e3a915fcf48646969a3b0a
Radoslav Genchev <r.genchew@gmail.com>
2018-07-26 14:18:12 +0200

    rename 'name' column to 'username' in users table

---
+++ db/migrate/20180726121705_rename_name_column_to_username_from_users.rb
@@ -0,0 +1,5 @@
+class RenameNameColumnToUsernameFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :name, :username
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_112859) do
+ActiveRecord::Schema.define(version: 2018_07_26_121705) do
 
   create_table "activities", force: :cascade do |t|
     t.integer "game_id"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2018_07_26_112859) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
