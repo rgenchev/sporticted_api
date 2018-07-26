2892c5b32efa8ff71fd54a50d1f6947fe8dcb4ac
Radoslav Genchev <r.genchew@gmail.com>
2018-07-26 14:11:21 +0200

    update schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_211618) do
+ActiveRecord::Schema.define(version: 2018_07_26_112859) do
 
-  create_table "challenges", force: :cascade do |t|
-    t.integer "winner_id"
-    t.integer "loser_id"
+  create_table "activities", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "challenge_id"
+    t.integer "result_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["loser_id"], name: "index_challenges_on_loser_id"
-    t.index ["winner_id"], name: "index_challenges_on_winner_id"
+    t.index ["challenge_id"], name: "index_activities_on_challenge_id"
+    t.index ["game_id"], name: "index_activities_on_game_id"
+    t.index ["result_id"], name: "index_activities_on_result_id"
+  end
+
+  create_table "challenges", force: :cascade do |t|
+    t.integer "challenger_id"
+    t.integer "challenged_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "place_id"
+    t.datetime "date"
+    t.boolean "is_confirmed", default: false
+    t.boolean "is_accepted", default: false
+    t.index ["challenged_id"], name: "index_challenges_on_challenged_id"
+    t.index ["challenger_id"], name: "index_challenges_on_challenger_id"
+    t.index ["place_id"], name: "index_challenges_on_place_id"
+  end
+
+  create_table "challenges_users", force: :cascade do |t|
+    t.integer "challenge_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["challenge_id"], name: "index_challenges_users_on_challenge_id"
+    t.index ["user_id"], name: "index_challenges_users_on_user_id"
   end
 
   create_table "games", force: :cascade do |t|
@@ -31,9 +56,7 @@ ActiveRecord::Schema.define(version: 2018_07_17_211618) do
   create_table "hosts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "place_id"
     t.integer "user_id"
-    t.index ["place_id"], name: "index_hosts_on_place_id"
     t.index ["user_id"], name: "index_hosts_on_user_id"
   end
 
@@ -44,13 +67,15 @@ ActiveRecord::Schema.define(version: 2018_07_17_211618) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "place_id"
+    t.index ["place_id"], name: "index_locations_on_place_id"
   end
 
   create_table "places", force: :cascade do |t|
-    t.integer "location_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["location_id"], name: "index_places_on_location_id"
+    t.integer "host_id"
+    t.index ["host_id"], name: "index_places_on_host_id"
   end
 
   create_table "records", force: :cascade do |t|
@@ -61,6 +86,15 @@ ActiveRecord::Schema.define(version: 2018_07_17_211618) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "results", force: :cascade do |t|
+    t.integer "winner_id"
+    t.integer "loser_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["loser_id"], name: "index_results_on_loser_id"
+    t.index ["winner_id"], name: "index_results_on_winner_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -68,6 +102,7 @@ ActiveRecord::Schema.define(version: 2018_07_17_211618) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
+    t.boolean "is_host", default: false
   end
 
 end
