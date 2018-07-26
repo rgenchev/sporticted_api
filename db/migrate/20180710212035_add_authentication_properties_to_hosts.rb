00f6b4a85240fba83ded8c19a3463a7fdc90d481
Radoslav Genchev <r.genchew@gmail.com>
2018-07-26 08:55:46 +0200

    remove needless migration

--- db/migrate/20180710212035_add_authentication_properties_to_hosts.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthenticationPropertiesToHosts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :hosts, :email, :string
-    add_column :hosts, :password_digest, :string
-    add_column :hosts, :name, :string
-  end
-end
