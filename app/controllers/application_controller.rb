0c52d5042438c18554371d83e1243e0b48a68178
Radoslav Genchev <r.genchew@gmail.com>
2018-07-27 14:44:40 +0200

    rescue from Pundit:NotAuthorizedError with a corresponding response status and message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::API
   before_action :authenticate_request
   attr_reader :current_user
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
 
   def authenticate_request
     @current_user = AuthorizeApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
+
+  def user_not_authorized
+    render json: { error: 'Not Authorized' }, status: 401
   end
 end
